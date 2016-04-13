176aff0de991f97e664b15a3b19b71aad2fc275d
mse2321 <mse233@gmail.com>
2016-04-13 02:32:52 -0400

    finished challenge

--- js/app.js
+++ js/app.js
@@ -1,65 +1,74 @@
 "use strict";
 
 $("#logo_form").submit(sendResults);
-//$(".disabled").click(dropDown);
+$(".disabled").click(dropDown);
+$(".first_select > .options").click(selection);
+$(".second_select > .options").click(secondSelection);
 
-/*function dropDown () {
+// displays select options
+function dropDown () {
 	$("li").css("display", "block");
 };
-*/
 
-function sendResults () {
+// sends data to sendResults of the selected item
+function selection () {
 
-	var results = $(this).serialize();
+	var program = $(this).text();
+	$(".first_select > .disabled").html(program + "<i class='fa fa-chevron-down' aria-hidden='true'></i>");
+	$(".first_select > .options").css("display", "none");
 
-	var indexStart = results.indexOf("=") + 1;
-	var indexEnd = results.indexOf("&");
-	var indexStartSecondary = results.lastIndexOf("=") + 1;
+	sendResults(program);
+}
 
-	var program = results.slice(indexStart, indexEnd);
-	var education = results.slice(indexStartSecondary);
+// sends data to sendResults of the second selected item
+function secondSelection () {
 
-	console.log(results);
-	console.log(indexStart);
-	console.log(indexEnd);
+	var education = $(this).text();
+	$(".second_select > .disabled").html(education + "<i class='fa fa-chevron-down' aria-hidden='true'></i>");
+	$(".second_select > .options").css("display", "none");
 
-	console.log(program);
-	console.log(education);
+	sendResults(education);
+}
 
+// runs a check to make sure the search button is clicked before executing
+function sendResults (program, education) {
 
-	displayProgramLogos(program);
-	displayEducationLogos(education);
+	$("#submit").click(function() {
+		displayProgramLogos(program);
+		displayEducationLogos(education);
+	})
 
 	return false;
-	
 };
 
+// displays logos based on program criteria
 function displayProgramLogos (program){
 
-	if(program == "SCHOLARSHIP+PROGRAM") {
+	if(program == "SCHOLARSHIP PROGRAM") {
 		$(".scholar").addClass("selected");
 		$(".loan").removeClass("selected");
 
-	} else if (program == "LOAN+PROGRAM") {
+	} else if (program == "LOAN PROGRAM") {
 		$(".loan").addClass("selected");
 		$(".scholar").removeClass("selected");
 
-	} else if (program == "SEARCH+ALL") {
+	} else if (program == "SEARCH ALL") {
 		$(".display").addClass("selected");
 	};
 };
 
+// displays logos based on education criteria
 function displayEducationLogos (education){
 
 	if(education == "KINGDERGARTEN") {
 		$(".k_garten").addClass("selected");
 		$(".h_school").removeClass("selected");
 
-	} else if (education == "HIGH+SCHOOL") {
+	} else if (education == "HIGH SCHOOL") {
 		$(".h_school").addClass("selected");
 		$(".k_garten").removeClass("selected");
 
-	} else if (education == "SEARCH+ALL") {
+	} else if (education == "SEARCH ALL") {
 		$("img").addClass("selected");
 	};
 };
